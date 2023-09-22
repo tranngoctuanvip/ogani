4e28bffdeef88a0f740e2991f9a4465539dc5a5e
tuantn <faker2kvip@gmail.com>
2023-09-22 09:03:50 +0700

    update file config version1

--- ogani_BE/src/main/java/com/example/ogani_be/Config/Config.java
+++ ogani_BE/src/main/java/com/example/ogani_be/Config/Config.java
@@ -19,60 +19,60 @@ public class Config {
     public static String secretKey = "YSUVDHKYNHULHLSZBPJMZVERKMCYQERR";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
-//    public static String md5(String message) {
-//        String digest = null;
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("MD5");
-//            byte[] hash = md.digest(message.getBytes("UTF-8"));
-//            StringBuilder sb = new StringBuilder(2 * hash.length);
-//            for (byte b : hash) {
-//                sb.append(String.format("%02x", b & 0xff));
-//            }
-//            digest = sb.toString();
-//        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
-//            digest = "";
-//        }
-//        return digest;
-//    }
+    public static String md5(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            digest = "";
+        }
+        return digest;
+    }
 
-//    public static String Sha256(String message) {
-//        String digest = null;
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("SHA-256");
-//            byte[] hash = md.digest(message.getBytes("UTF-8"));
-//            StringBuilder sb = new StringBuilder(2 * hash.length);
-//            for (byte b : hash) {
-//                sb.append(String.format("%02x", b & 0xff));
-//            }
-//            digest = sb.toString();
-//        } catch (UnsupportedEncodingException ex) {
-//            digest = "";
-//        } catch (NoSuchAlgorithmException ex) {
-//            digest = "";
-//        }
-//        return digest;
-//    }
+    public static String Sha256(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException ex) {
+            digest = "";
+        } catch (NoSuchAlgorithmException ex) {
+            digest = "";
+        }
+        return digest;
+    }
 
-    //Util for VNPAY
-//    public static String hashAllFields(Map fields) {
-//        List fieldNames = new ArrayList(fields.keySet());
-//        Collections.sort(fieldNames);
-//        StringBuilder sb = new StringBuilder();
-//        Iterator itr = fieldNames.iterator();
-//        while (itr.hasNext()) {
-//            String fieldName = (String) itr.next();
-//            String fieldValue = (String) fields.get(fieldName);
-//            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-//                sb.append(fieldName);
-//                sb.append("=");
-//                sb.append(fieldValue);
-//            }
-//            if (itr.hasNext()) {
-//                sb.append("&");
-//            }
-//        }
-//        return hmacSHA512(secretKey,sb.toString());
-//    }
+    Util for VNPAY
+    public static String hashAllFields(Map fields) {
+        List fieldNames = new ArrayList(fields.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder sb = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) fields.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                sb.append(fieldName);
+                sb.append("=");
+                sb.append(fieldValue);
+            }
+            if (itr.hasNext()) {
+                sb.append("&");
+            }
+        }
+        return hmacSHA512(secretKey,sb.toString());
+    }
 
     public static String hmacSHA512(final String key, final String data) {
         try {
